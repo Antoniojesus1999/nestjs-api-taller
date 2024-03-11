e6a930fb86d7703e3eaa57f004cf42b24391e9e8
Antonio Jesús <antoniojesuspv99@gmail.com>
2024-03-11 22:58:22 +0100

    añadiendo constructor a client.dto.ts

--- src/contexts/client/dtos/client.dto.ts
+++ src/contexts/client/dtos/client.dto.ts
@@ -1,26 +1,43 @@
+import { IsEmail, IsNotEmpty, IsOptional } from "class-validator";
+
 import { Vehicle } from "../schemas/vehicle.schema";
-import { IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
-export class ClientDto{
-    @IsOptional()
-    name:String;
-    
-    @IsNotEmpty()
-    nif:String;
+export class ClientDto {
+  @IsOptional()
+  name: string;
 
-    @IsOptional()
-    surName1:String;
+  @IsNotEmpty()
+  nif: string;
 
-    @IsOptional()
-    surName2:String;
-    
-    @IsOptional()
-    tlfn: String;
+  @IsOptional()
+  surName1: string;
 
-    @IsNotEmpty()
-    @IsEmail()
-    email:String;
+  @IsOptional()
+  surName2: string;
 
-    @IsOptional()
-    cars:[Vehicle]
-    
+  @IsOptional()
+  tlfn: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  cars: [Vehicle];
+  constructor(
+    name: string,
+    nif: string,
+    surName1: string,
+    surName2: string,
+    tlfn: string,
+    email: string,
+    cars: [Vehicle],
+  ) {
+    this.name = name;
+    this.nif = nif;
+    this.surName1 = surName1;
+    this.surName2 = surName2;
+    this.tlfn = tlfn;
+    this.email = email;
+    this.cars = cars;
+  }
 }
