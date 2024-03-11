4468f7a12019165f6fde0d921b6fccc27279430f
Antonio Jesús <antoniojesuspv99@gmail.com>
2024-03-11 23:13:51 +0100

    arreglando pdf.service.ts

--- src/contexts/pdf/pdf.service.ts
+++ src/contexts/pdf/pdf.service.ts
@@ -37,97 +37,97 @@ export class PdfService {
       });
 
       const nombreTaller = form.getTextField("nombreTaller");
-      nombreTaller.setText(dataPdfReq.nombreTaller as string);
+      nombreTaller.setText(dataPdfReq.nombreTaller);
       const da = nombreTaller.acroField.getDefaultAppearance() ?? "";
       const newDa = da + setFillingRgbColor(0, 0, 0).toString();
       nombreTaller.acroField.setDefaultAppearance(newDa);
 
       const cpTaller = form.getTextField("cpTaller");
-      cpTaller.setText(dataPdfReq.cpTaller as string);
+      cpTaller.setText(dataPdfReq.cpTaller);
       cpTaller.acroField.setDefaultAppearance(newDa);
 
       const munYProvinTaller = form.getTextField("munYProvinTaller");
-      munYProvinTaller.setText(dataPdfReq.munYProvinTaller as string);
+      munYProvinTaller.setText(dataPdfReq.munYProvinTaller);
       munYProvinTaller.acroField.setDefaultAppearance(newDa);
 
       const nifTaller = form.getTextField("nifTaller");
-      nifTaller.setText(dataPdfReq.nifTaller as string);
+      nifTaller.setText(dataPdfReq.nifTaller);
       nifTaller.acroField.setDefaultAppearance(newDa);
 
       const riiaTaller = form.getTextField("riiaTaller");
-      riiaTaller.setText(dataPdfReq.riiaTaller as string);
+      riiaTaller.setText(dataPdfReq.riiaTaller);
       riiaTaller.acroField.setDefaultAppearance(newDa);
 
       const tlfTaller = form.getTextField("tlfTaller");
-      tlfTaller.setText(dataPdfReq.tlfTaller as string);
+      tlfTaller.setText(dataPdfReq.tlfTaller);
       tlfTaller.acroField.setDefaultAppearance(newDa);
 
       const numResgDepoTaller = form.getTextField("numResgDepoTaller");
-      numResgDepoTaller.setText(dataPdfReq.numResgDepoTaller as string);
+      numResgDepoTaller.setText(dataPdfReq.numResgDepoTaller);
       numResgDepoTaller.acroField.setDefaultAppearance(newDa);
 
       const matriculaVeh = form.getTextField("matriculaVeh");
-      matriculaVeh.setText(dataPdfReq.matriculaVeh as string);
+      matriculaVeh.setText(dataPdfReq.matriculaVeh);
       matriculaVeh.acroField.setDefaultAppearance(newDa);
 
       const marcaModeloVeh = form.getTextField("marcaModeloVeh");
-      marcaModeloVeh.setText(dataPdfReq.marcaModeloVeh as string);
+      marcaModeloVeh.setText(dataPdfReq.marcaModeloVeh);
       marcaModeloVeh.acroField.setDefaultAppearance(newDa);
 
       const combustibleVeh = form.getTextField("combustibleVeh");
-      combustibleVeh.setText(dataPdfReq.combustibleVeh as string);
+      combustibleVeh.setText(dataPdfReq.combustibleVeh);
       combustibleVeh.acroField.setDefaultAppearance(newDa);
 
       const seguroVeh = form.getTextField("seguroVeh");
-      seguroVeh.setText(dataPdfReq.seguroVeh as string);
+      seguroVeh.setText(dataPdfReq.seguroVeh);
       seguroVeh.acroField.setDefaultAppearance(newDa);
 
       const polizaVeh = form.getTextField("polizaVeh");
-      polizaVeh.setText(dataPdfReq.polizaVeh as string);
+      polizaVeh.setText(dataPdfReq.polizaVeh);
       polizaVeh.acroField.setDefaultAppearance(newDa);
 
       const kmsVeh = form.getTextField("kmsVeh");
-      kmsVeh.setText(dataPdfReq.kmsVeh as string);
+      kmsVeh.setText(dataPdfReq.kmsVeh);
       kmsVeh.acroField.setDefaultAppearance(newDa);
 
       const chasisVeh = form.getTextField("chasisVeh");
-      chasisVeh.setText(dataPdfReq.chasisVeh as string);
+      chasisVeh.setText(dataPdfReq.chasisVeh);
       chasisVeh.acroField.setDefaultAppearance(newDa);
 
       const ape1Cliente_2 = form.getTextField("ape1Cliente_2");
-      ape1Cliente_2.setText(dataPdfReq.ape1Cliente_2 as string);
+      ape1Cliente_2.setText(dataPdfReq.ape1Cliente_2);
       ape1Cliente_2.acroField.setDefaultAppearance(newDa);
 
       const fecEntradaVeh = form.getTextField("fecEntradaVeh");
-      fecEntradaVeh.setText(dataPdfReq.fecEntradaVeh as string);
+      fecEntradaVeh.setText(dataPdfReq.fecEntradaVeh);
       fecEntradaVeh.acroField.setDefaultAppearance(newDa);
 
       const fecSalidaVeh = form.getTextField("fecSalidaVeh");
-      fecSalidaVeh.setText(dataPdfReq.fecSalidaVeh as string);
+      fecSalidaVeh.setText(dataPdfReq.fecSalidaVeh);
       fecSalidaVeh.acroField.setDefaultAppearance(newDa);
 
       const nomCliente = form.getTextField("nomCliente");
-      nomCliente.setText(dataPdfReq.nomCliente as string);
+      nomCliente.setText(dataPdfReq.nomCliente);
       nomCliente.acroField.setDefaultAppearance(newDa);
 
       const ape1Cliente = form.getTextField("ape1Cliente");
-      ape1Cliente.setText(dataPdfReq.ape1Cliente as string);
+      ape1Cliente.setText(dataPdfReq.ape1Cliente);
       ape1Cliente.acroField.setDefaultAppearance(newDa);
 
       const nifCliente = form.getTextField("nifCliente");
-      nifCliente.setText(dataPdfReq.nifCliente as string);
+      nifCliente.setText(dataPdfReq.nifCliente);
       nifCliente.acroField.setDefaultAppearance(newDa);
 
       const emailCliente = form.getTextField("emailCliente");
-      emailCliente.setText(dataPdfReq.emailCliente as string);
+      emailCliente.setText(dataPdfReq.emailCliente);
       emailCliente.acroField.setDefaultAppearance(newDa);
 
       const tlfCliente = form.getTextField("tlfCliente");
-      tlfCliente.setText(dataPdfReq.tlfCliente as string);
+      tlfCliente.setText(dataPdfReq.tlfCliente);
       tlfCliente.acroField.setDefaultAppearance(newDa);
 
       const tareasRealizadas = form.getTextField("tareasRealizadas");
-      tareasRealizadas.setText(dataPdfReq.tareasRealizadas as string);
+      tareasRealizadas.setText(dataPdfReq.tareasRealizadas);
       tareasRealizadas.acroField.setDefaultAppearance(newDa);
 
       const pdfBytes = await pdfDoc.save();
