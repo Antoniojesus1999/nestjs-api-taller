334e22f22270a67d29ba9bb0809ba5e94d833bfb
Antonio Jesús Ponce Vela <antoniojesuspv99@gmail.com>
2024-10-24 17:10:35 +0200

    Quitando dependencia mapper transformer

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import { Logger, ValidationPipe } from "@nestjs/common";
+import { Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
-import { useContainer } from "class-validator";
 
 import { AppModule } from "./app.module";
 
@@ -18,18 +17,6 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<string>("PORT", "3000");
   const ipServer = configService.get<string>("IP_SERVER", "172.0.0.5");
-
-  // Configurar el uso de ValidationPipe globalmente
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true, // Habilitar la transformación automática de DTOs
-      whitelist: true, // Eliminar propiedades no declaradas en los DTOs
-      forbidNonWhitelisted: true, // Lanzar un error si se encuentran propiedades no declaradas
-    }),
-  );
-
-  // Configurar class-validator para usar el contenedor de NestJS
-  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   await app.listen(port, ipServer);
 
